82c81f40a98060828c50d7d7025dea331426f034
pranjay-poddar <pranjaypoddar@outlook.com>
2021-02-27 03:47:57 +0530

    completed academics and added carousel in campus life and research

--- src/app/campus/campus.component.ts
+++ src/app/campus/campus.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-campus',
   templateUrl: './campus.component.html',
@@ -8,8 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class CampusComponent implements OnInit {
 
   constructor() { }
-
+  title = 'myJquery';
   ngOnInit(): void {
+    $('#recipeCarousel').carousel({
+      interval: 10000
+    })
+    
+    $('.carousel .carousel-item').each(function(){
+        var minPerSlide = 3;
+        var next = $(this).next();
+        if (!next.length) {
+        next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<minPerSlide;i++) {
+            next=next.next();
+            if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+            
+            next.children(':first-child').clone().appendTo($(this));
+          }
+    });
+    
   }
 
 }
+
+
+
