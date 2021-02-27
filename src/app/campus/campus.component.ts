5f3ebd7bb635fd5aefeb19a90b2f5ad6d614b50d
pranjay-poddar <pranjaypoddar@outlook.com>
2021-02-27 20:26:04 +0530

    added campus research and academic sections and made responsive

--- src/app/campus/campus.component.ts
+++ src/app/campus/campus.component.ts
@@ -8,30 +8,11 @@ declare var $: any;
 export class CampusComponent implements OnInit {
 
   constructor() { }
-  title = 'myJquery';
   ngOnInit(): void {
-    $('#recipeCarousel').carousel({
-      interval: 10000
-    })
-    
-    $('.carousel .carousel-item').each(function(){
-        var minPerSlide = 3;
-        var next = $(this).next();
-        if (!next.length) {
-        next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-        
-        for (var i=0;i<minPerSlide;i++) {
-            next=next.next();
-            if (!next.length) {
-              next = $(this).siblings(':first');
-            }
-            
-            next.children(':first-child').clone().appendTo($(this));
-          }
-    });
-    
+    $('#blogCarousel1').carousel({
+      interval: 5000
+  });
+
   }
 
 }
