19f7d4e4a7e9fead31e1c9642cc37d5ca1e83547
shreyanshp25 <shreyansh2511@gmail.com>
2021-02-28 10:42:05 +0530

    admission international section added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     $(window).scroll(function(){
-      $('nav').toggleClass('scrolled', $(this).scrollTop() > 10);
+      $('nav').toggleClass('scrolled', $(this).scrollTop() > 200);
   });
   }
 
