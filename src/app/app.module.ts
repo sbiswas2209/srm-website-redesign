5a484c2262a1adf6980fd5c660a7ddfbbeaaf07f
shreyanshp25 <shreyansh2511@gmail.com>
2021-03-14 18:50:17 +0530

    merged main branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { AdmissionComponent } from './admission/admission.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     AdmissionComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
